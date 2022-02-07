eeb0d9fd1d8964fdb53cf12f95b2d0b39c90d6e6
Hunter Osmun <Hunter@Osmun.net>
2022-02-07 16:51:43 -0700

    showing Chance how to commit through terminal

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App () {
     <div>
       <h1>Chance is dah best!!</h1>
       <h3>Here is his example website</h3>
+      <p>
+        Hunter is so cool for showing Chance all of this stuff... But Chance is
+        even cooler for putting up with Hunter's crazy
+        <h3>Shenanigans</h3>
+      </p>
     </div>
   )
 }
